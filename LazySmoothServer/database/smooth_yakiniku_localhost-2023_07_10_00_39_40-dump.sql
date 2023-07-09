9eac33e3ff6f9d0db6e2efdb3f143d884a5ebfda
MrGOStepz <mrgostepz@gmail.com>
2023-07-10 00:40:47 +0700

    SQL

---
+++ LazySmoothServer/database/smooth_yakiniku_localhost-2023_07_10_00_39_40-dump.sql
@@ -0,0 +1,173 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: lazy_smooth_pos
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category_info`
+--
+
+DROP TABLE IF EXISTS `category_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category_info` (
+  `category_info_id` bigint NOT NULL AUTO_INCREMENT,
+  `image_path` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`category_info_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category_info`
+--
+
+LOCK TABLES `category_info` WRITE;
+/*!40000 ALTER TABLE `category_info` DISABLE KEYS */;
+INSERT INTO `category_info` VALUES (3,'8.png','หมวดอุด้งร้อน'),(4,'7.png','หมวดอุด้งเย็นๆ ร้อนๆ'),(5,'7.png','หมวดอุด้งร้อน');
+/*!40000 ALTER TABLE `category_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_info`
+--
+
+DROP TABLE IF EXISTS `order_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_info` (
+  `id` bigint NOT NULL,
+  `amount` double DEFAULT NULL,
+  `last_updated_time` datetime(6) DEFAULT NULL,
+  `order_type` varchar(255) DEFAULT NULL,
+  `receipt_json` varchar(255) DEFAULT NULL,
+  `started_time` datetime(6) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `table_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_info`
+--
+
+LOCK TABLES `order_info` WRITE;
+/*!40000 ALTER TABLE `order_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_item`
+--
+
+DROP TABLE IF EXISTS `order_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_item` (
+  `id` bigint NOT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `last_updated_time` datetime(6) DEFAULT NULL,
+  `order_info_id` bigint DEFAULT NULL,
+  `popup_detail_id` bigint DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `product_id` bigint DEFAULT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `started_time` datetime(6) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_item`
+--
+
+LOCK TABLES `order_item` WRITE;
+/*!40000 ALTER TABLE `order_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `product_id` bigint NOT NULL,
+  `category_info_id` bigint DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `food_type` varchar(255) DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `is_available` bit(1) DEFAULT NULL,
+  `location_column` int DEFAULT NULL,
+  `location_page` int DEFAULT NULL,
+  `location_row` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `popup_info_id` bigint DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `stock` int DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `table_info`
+--
+
+DROP TABLE IF EXISTS `table_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `table_info` (
+  `table_info_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `order_info_id` int DEFAULT NULL,
+  PRIMARY KEY (`table_info_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table_info`
+--
+
+LOCK TABLES `table_info` WRITE;
+/*!40000 ALTER TABLE `table_info` DISABLE KEYS */;
+INSERT INTO `table_info` VALUES (1,'1','COOK',0),(2,'2','FREE',0),(3,'3','COOK',0),(4,'4','FREE',0),(5,'5','FREE',0);
+/*!40000 ALTER TABLE `table_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-10  0:39:40
