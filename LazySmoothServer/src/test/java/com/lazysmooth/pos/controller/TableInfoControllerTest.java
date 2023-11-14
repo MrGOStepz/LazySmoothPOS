fe46c2cada5c2163ef4ff22ffdf44730fd0d4650
Warawat Suksakulwat <MrGOStepz@gmail.com>
2023-11-15 00:15:27 +0700

    [LS] - Update Database Password

--- LazySmoothServer/src/test/java/com/lazysmooth/pos/controller/TableInfoControllerTest.java
+++ LazySmoothServer/src/test/java/com/lazysmooth/pos/controller/TableInfoControllerTest.java
@@ -1,4 +1,17 @@
 package com.lazysmooth.pos.controller;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TableInfoControllerTest {
+
+    ProductController productController;
+
+    void setup() {
+    }
+    @Test
+    void testAddProduct() {
+
+    }
 }
